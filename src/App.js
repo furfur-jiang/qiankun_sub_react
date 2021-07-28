819af62b8d39de816719b2473594537068328cfc
jiangmaoqiong <2282995214@eqq.com>
2021-07-28 11:39:57 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+//下载react-app-rewired
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={'/react'}>
+      <Link to='/'>首页</Link>
+      <Link to="/about">关于页面</Link>
+        <Route path="/" exact render={()=>(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        )}></Route>
+        <Route path="/about" render={()=>(
+            <div>react about</div>
+        )}></Route>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(){
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
 
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+// if(window.__POWERED_BY_QIANKUN__){ //动态添加publicPath
+//     __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+// }
+if(!window.__POWERED_BY_QIANKUN__){//默认独立运行
+    render()
+}
+
+export async function bootstrap(){}
+
+export async function mount(){
+    render()
+}
+
+export async function unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'))
+}
